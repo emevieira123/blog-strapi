09e53cab0d7bdb8c0fd97088a6a4df6827221107
Emerson Vieira <33058881+emevieira123@users.noreply.github.com>
2022-03-01 22:38:11 -0400

    Update

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -1,19 +1,29 @@
-const parse = require('pg-connection-string').parse;
-const config = parse(process.env.DATABASE_URL);
-
-module.exports = ({ env }) => ({
-  connection: {
-    client: 'postgres',
-    connection: {
-      host: config.host,
-      port: config.port,
-      database: config.database,
-      user: config.user,
-      password: config.password,
-      ssl: {
-        rejectUnauthorized: false
+const { parse } = require("pg-connection-string");
+ 
+module.exports = ({ env }) => {
+  const { host, port, database, user, password } = parse(env("DATABASE_URL"));
+ 
+  return {
+    defaultConnection: "default",
+    connections: {
+      default: {
+        connector: "bookshelf",
+        settings: {
+          client: "postgres",
+          host,
+          port,
+          database,
+          username: user,
+          password,
+          ssl: {
+            // For self-signed certificates
+            rejectUnauthorized: env.bool("DATABASE_SSL_SELF", false), 
+          },
+          options: {
+            ssl: true,
+          },
+        },
       },
     },
-    debug: false,
-  },
-});
+  };
+};
